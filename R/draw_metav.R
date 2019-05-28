38deb9ccb10424b00dde1577bcb1d1dfb210ee60
Bionett <cesar.prada@usp.br>
2019-05-28 15:37:17 -0300

    REM topconfects

--- R/draw_metav.R
+++ R/draw_metav.R
@@ -5,28 +5,43 @@
 #' @param jobname name of the running job <string>
 #' @param outputfolder /path where to write the results/ <string>
 #' @param genecol column name of the variable to label genes in the .html file <string>
+#' @param metathr top percentage of perturbed genes to be highlighted <double>
+#' @param either 'PDF' or 'HTML' to save metaolcano as .pdf or .html respectively <string>
 #' @keywords write 'combining meta-analysis' metavolcano
 #' @export
 #' @examples
 #' draw.metavolcano.metap()
-draw.metav <- function(meta_res, jobname, outputfolder, genecol) {
+draw.metav <- function(meta_res, jobname, outputfolder, genecol, metathr, draw) {
   #meta_res <- meta_res %>%
   #  mutate(geninfo = paste(c(!!as.name(genecol),
   #                           paste0('#deg: ', ndeg),
   #                           paste0('#ddeg: ', ddeg)),
   #                         collapse = '\n'))
-  gg <- ggplotly(
-    ggplot(arrange(meta_res, abs(randomSummary)),
-           aes(x = randomSummary, y = -log10(randomP), color = dircon, text = !!rlang::sym(genecol))) +
-      geom_point() +
-      scale_color_gradient2(midpoint=0, low="blue", mid="white", high="red") +
-      geom_errorbarh(aes(xmax = randomCi.ub, xmin = randomCi.lb, color = dircon)) +
-      theme_classic() +
-      theme(panel.border= element_blank()) +
-      theme(axis.text.x = element_text(angle = 0, hjust = 1)) +
-      theme(axis.line.x = element_line(color = "black", size = 0.6, lineend = "square"),
-            axis.line.y = element_line(color = "black", size = 0.6, lineend = "square"))
-  )
-  htmlwidgets::saveWidget(as_widget(gg), paste0(normalizePath(outputfolder),
-                                                "/RandomEffectModel_MetaVolcano_", jobname, ".html"))
-}
+  
+  meta_res %>%
+	  mutate(dircon2 = ifelse(rank <= quantile(meta_res[['rank']], metathr), dircon, NA)) %>%
+	  filter(rank >  quantile(meta_res[['rank']], 0.6)) -> meta_res
+
+  gg <- ggplot(arrange(meta_res, abs(randomSummary)),
+	       aes(x = randomSummary, y = -log10(randomP), color = dircon2, text = !!rlang::sym(genecol))) +
+	      geom_point() +
+	      scale_color_gradient2(midpoint=0, low="blue", mid="white", high="red") +
+	      geom_errorbarh(aes(xmax = randomCi.ub, xmin = randomCi.lb, color = dircon)) +
+	      theme_classic() +
+	      theme(panel.border= element_blank()) +
+	      theme(axis.text.x = element_text(angle = 0, hjust = 1)) +
+	      theme(axis.line.x = element_line(color = "black", size = 0.6, lineend = "square"),
+	            axis.line.y = element_line(color = "black", size = 0.6, lineend = "square"))
+
+  if(draw == "PDF") {
+
+	  pdf(paste0(normalizePath(outputfolder), "/RandomEffectModel_MetaVolcano_", jobname, ".pdf"), width = 7, height = 10)
+		  plot(gg)
+	  dev.off()
+  
+  } else if (draw == "HTML") {
+
+	  htmlwidgets::saveWidget(as_widget(ggplotly(gg)), paste0(normalizePath(outputfolder),
+							"/RandomEffectModel_MetaVolcano_", jobname, ".html"))
+  }	      
+ }
